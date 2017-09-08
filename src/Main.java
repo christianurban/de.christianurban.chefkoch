0e6eee25cfdf99572ff3958e83c33a60dbb3c174
christianurban <urban.christian@web.de>
2017-09-08 14:43:35 +0200

    Output to *.sql

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.awt.*;
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.*;
 /**
  * Created by christianurban on 02.11.16.
  */
@@ -13,14 +16,50 @@ public class Main {
             //reader.ReadHtmlFromUrl("http://www.chefkoch.de/rezepte/2558081400347453/Kartoffelsuppe-mit-Erbsen-und-Mettwuerstchen.html");
             Recipe r = reader.ReadHtmlFromUrl("http://www.chefkoch.de/rezepte/901131195473455/Mediterranes-Ofengemuese.html");
 
+            int recipedId = 2;
+
+
             StringBuilder sb = new StringBuilder();
-            sb.append("\"2\",\""+r.getTitle()+"\",\"");
+            sb.append("INSERT INTO recipe (ID,Title,Preparation,ImageUrl) VALUES(");
+            sb.append(recipedId+",\""+StringEscapeUtils.escapeHtml4(r.getTitle())+"\",\"");
             for(String i : r.getInstructions())
             {
-                //i = i.replace("°C","Grad Celsius");
-                sb.append(i + "<br/>");
+                sb.append(StringEscapeUtils.escapeHtml4(i) + "<br/>");
             }
-            sb.append("\",\"\"");
+            sb.append("\",\"\");\n");
+
+            for (Ingredient i : r.getIngredients()) {
+                sb.append("INSERT INTO recipe (RecipeID,ID,Quantity,Unit,Name) VALUES(");
+                sb.append(recipedId+",NULL,");
+                String test = i.getQuantity().replaceAll("\u00A0"," ");
+                test = test.trim();
+                String[] quantityArray = test.trim().split("\\s+");
+                if(quantityArray.length == 1){
+                    String quantityStr = quantityArray[0];
+                    String unitString = "NULL";
+                    if(StringUtils.isEmpty(quantityStr)) {
+                        quantityStr = "NULL";
+                    }
+                    if(!quantityStr.matches("\\d*\\.?\\d+")){
+                        quantityStr = "NULL";
+                    }
+                    else{
+                        unitString = "\"STÜCK\"";
+                    }
+                    sb.append(quantityStr+","+unitString+",");
+                }
+                if(quantityArray.length == 2){
+                    String quantityStr = quantityArray[0];
+                    String unitStr = quantityArray[1];
+                    if(quantityStr.matches("\\d*\\.?\\d+") && StringUtils.isEmpty(unitStr)){
+                        unitStr = "\"STÜCK\"";
+                    }
+                    sb.append(quantityStr+",\""+unitStr+"\",");
+                }
+                sb.append(("\""+i.getName()+"\""));
+                sb.append(");\n");
+                System.out.println(i.getQuantity() + " - " + i.getName());
+            }
 
 
 
@@ -67,13 +106,13 @@ public class Main {
             sb.append("</body>\n");
             sb.append("</html>");*/
 
-            PrintWriter pw = new PrintWriter("test.csv","ISO-8859-1");
+            PrintWriter pw = new PrintWriter("test.sql","ISO-8859-1");
             pw.write(sb.toString());
             pw.flush();
             pw.close();
 
 
-            File htmlFile = new File("test.csv");
+            //File htmlFile = new File("test.csv");
             //Desktop.getDesktop().browse(htmlFile.toURI());
         }
         catch (Exception ex)
